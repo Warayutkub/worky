29f31cc8daf05592b710c77dd8428d2cd0dc043b
Warayut <xshark0631673569@gmail.com>
2026-08-25 01:10:19 +0700

    fix route

--- action/navAction.js
+++ action/navAction.js
@@ -1,15 +1,15 @@
 const btn_array = [
     {
         btn : document.getElementById('btn-to-history'),
-        to : "./history/"
+        to : "history"
     },
     {
         btn : document.getElementById('btn-to-timeSave'),
-        to : "./timeSave/"
+        to : "timeSave"
     },
     {
         btn : document.getElementById('btn-to-home'),
-        to : "./"
+        to : ""
     },
     
     
